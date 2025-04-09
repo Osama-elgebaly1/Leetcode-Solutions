581d42c59ed88dec1b876c266788d2fd51e8425b
Osama-elgebaly1 <osamaelgebaly78@gmail.com>
2025-04-09 18:36:23 +0200

    Update 1480-running-sum-of-1d-array.py

--- Easy/1480-running-sum-of-1d-array.py
+++ Easy/1480-running-sum-of-1d-array.py
@@ -1,18 +1,17 @@
 # 1480. Running Sum of 1d Array
 # Problem: Given an array nums, return an array such that each element at index i 
 # is the sum of all the elements from index 0 to i (inclusive).
-
 class Solution(object):
     def runningSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        # Iterate through the list starting from index 1
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]  # Add the previous element to the current one
+        results = []  # List to store running sum results
+        total = 0  # Variable to track the cumulative sum
         
-        return nums  # Return the updated list
-
-s1 = Solution()
-print(s1.runningSum([1,2,3]))
+        for num in nums:  # Iterate through the input list
+            total += num  # Add current number to the cumulative sum
+            results.append(total)  # Append the current cumulative sum to the results
+        
+        return results  # Return the final list of running sums
